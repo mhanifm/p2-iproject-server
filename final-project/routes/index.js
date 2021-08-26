162db187e85123eb2d58e1de6f58ae7cb3fd880d
mhanifm <mhanifmuttaqin95@gmail.com>
2021-08-27 01:47:49 +0700

    add router flight in index router

--- final-project/routes/index.js
+++ final-project/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const userRouter = require('./users')
+const flightRouter = require('./flights')
 const weatherRouter = require('./weathers')
 
 router.use('/', userRouter)
+router.use('/flights', flightRouter)
 router.use('/weathers', weatherRouter)
 
 module.exports = router
